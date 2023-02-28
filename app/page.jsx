ca2a7e84f7f654c4830cff624af631c224cbc4b0
Justin <sunnysidedigitals@gmail.com>
2023-02-28 22:29:44 +0800

    remove content

--- app/page.jsx
+++ app/page.jsx
@@ -5,9 +5,9 @@ import Brands from "@/components/Brands";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
+// import Pricing from "@/components/Pricing";
+// import Testimonials from "@/components/Testimonials";
+// import Video from "@/components/Video";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
